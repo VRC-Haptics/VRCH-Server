e16506b20429a72e2712eb4d8bd81ab22f4e20a1
VirtuallyAverage <virtuallyaverage@gmail.com>
2025-02-07 01:03:49 -0600

    remove tracker sidecar

--- src-tauri/src/haptic/mdns.rs
+++ src-tauri/src/haptic/mdns.rs
@@ -1,110 +1,71 @@
-use std::collections::HashMap;
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
-use std::sync::{Arc, Mutex};
-use std::thread;
+use std::{sync::{Arc, Mutex}, time::Duration};
 use tauri::{AppHandle, Emitter};
-use std::os::windows::process::CommandExt;
-use winapi::um::winbase;
+use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
 
 use crate::haptic::Device;
 use crate::recall_device_group;
-use crate::vrc::Parameters;
-use serde_json::Value;
 
 pub fn start_device_listener(
     app_handle: AppHandle,
     devices_state: tauri::State<'_, Arc<Mutex<Vec<Device>>>>,
-    pid_state: tauri::State<'_, Arc<Mutex<u32>>>,
+    refresh_delay: u64,
 ) {
-    let mut cmd = Command::new("sidecars/tracker-sidecar.exe")
-        .arg("0")
-        .arg("_haptics._udp.local")
-        .arg("--debug")
-        .stdout(Stdio::piped())
-        .creation_flags(winbase::CREATE_NO_WINDOW)
-        .spawn()
-        .expect("Failed to execute command");
-    {
-        let mut pid = pid_state.lock().unwrap();
-        *pid = cmd.id();
-    }
+
     let devices = devices_state.inner().clone();
 
-    thread::spawn(move || {
-        let stdout = cmd.stdout.take().unwrap();
-        let reader = BufReader::new(stdout);
+    std::thread::spawn(move || {
+        let daemon = ServiceDaemon::new().expect("Failed to create daemon");
+        let reciever = daemon.browse("_haptics._udp.local.").unwrap();
 
-        for line in reader.lines() {
-            let long_raw = line.unwrap();
-            let split = long_raw.splitn(2, ":");
-            let log_type = split.to_owned().nth(0).unwrap();
-            if !log_type.starts_with("_") {
-                println!("{}", long_raw);
-                continue;
-            }
-
-            // split off debug message
-            let raw = split.to_owned().nth(1).unwrap();
-            let device = make_new_device(raw, &app_handle);
-
-            let mut devices = devices.lock().unwrap();
-            match log_type {
-                "_ADD" => {
-                    devices.push(device.clone());
-                    println!("device added: {:?}", device);
-                    app_handle.emit("device-added", device).unwrap();
+        loop {
+            if let Ok(event) = reciever.recv_timeout(Duration::from_secs(refresh_delay)) {
+                match event {
+                    ServiceEvent::ServiceResolved(info) => {
+                        let mut lock = devices.lock().unwrap();
+                        println!("Added device: {}", info.get_fullname());
+                        let built_device = info_to_device(info, &app_handle);
+                        app_handle.emit("device-added", built_device.clone()).unwrap();
+                        lock.push(built_device);
+                    }
+                    ServiceEvent::ServiceRemoved(_, full_name) => {
+                        let mut lock = devices.lock().unwrap();
+                        if let Some(index) = lock.iter().position(|device| device.full_name == full_name) {
+                            let device = lock.remove(index);
+                            println!("removing device: {}@{}", device.display_name, device.ip);
+                            // Emit the removed device
+                            app_handle.emit("device-removed", device).unwrap();
+                        }
+                    }
+                    _ => ()
                 }
-                "_RMV" => {
-                    devices.retain(|d| d.mac != device.mac);
-                    app_handle.emit("device-removed", device).unwrap();
+            } else {
+                let devices_lock = devices.lock().expect("couldn't get lock on services");
+                for service in devices_lock.iter() {
+                    let name = &service.full_name;
+                    let response = daemon.verify(name.to_string(), Duration::from_secs(2));
+                    match response {
+                        Ok(_) => (),
+                        Err(error) => match_error(error),
+                    }
                 }
-                "_DBUG" => println!("Debug messsage from sidecar: {}", raw),
-                &_ => println!(
-                    "Encountered unknown log type for mdns sidecar: {:#?}",
-                    devices
-                ),
             }
         }
     });
 }
 
-fn make_new_device(raw: &str, app_handle: &tauri::AppHandle) -> Device {
-    let parsed: Value = serde_json::from_str(raw).unwrap();
+fn match_error(err: mdns_sd::Error) {
+    match err {
+        mdns_sd::Error::Again => println!("Couldn't send query for service"),
+        mdns_sd::Error::Msg(msg) => println!("verify failed with messge: {}", msg),
+        mdns_sd::Error::ParseIpAddr(msg) => println!("Failed to parse ip adress: {}", msg),
+        _ => println!("Unknown error verifying"),
+    }
+}
 
-    // Extract fields required by Device::new()
-    let mac = parsed
-        .get("MAC")
-        .and_then(Value::as_str)
-        .unwrap()
-        .to_string();
-    let ip = parsed
-        .get("IP")
-        .and_then(Value::as_str)
-        .unwrap()
-        .to_string();
-    let display_name = parsed
-        .get("DisplayName")
-        .and_then(Value::as_str)
-        .unwrap()
-        .to_string();
-    let port = parsed.get("Port").and_then(Value::as_u64).unwrap() as u16;
-    let ttl = parsed.get("TTL").and_then(Value::as_u64).unwrap() as u32;
-
-    let mut new_device = Device {
-        mac: mac,
-        ip: ip,
-        display_name: display_name,
-        port: port,
-        ttl: ttl,
-        addr_groups: Vec::new(),
-        num_motors: 0,
-        been_pinged: false,
-        param_index: Vec::new(),
-        cached_param: HashMap::new(),
-        cached_menu: Parameters::new(),
-    };
-
+fn info_to_device(info: ServiceInfo, app_handle: &tauri::AppHandle) -> Device {
+    let mac = info.get_property_val_str("MAC").expect("couldn't get MAC records");
+    let ip = info.get_addresses_v4().iter().next().unwrap().to_string();
+    let mut new_device = Device::new(mac.to_string(), ip, info.get_port(), info.get_host_ttl(), info.get_fullname().to_string());
     // Try to recall saved groups
     if let Some(old_groups) = recall_device_group(app_handle, &new_device.mac) {
         new_device.addr_groups.extend(old_groups);
