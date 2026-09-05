738b375be2d4022c65d76449952abba473fd059b
Average <virtuallyaverage@gmail.com>
2026-09-05 06:03:03 -0400

    Updating workflows

--- src-vrc-oscquery/listen-for-vrc/build.sh
+++ src-vrc-oscquery/listen-for-vrc/build.sh
@@ -6,7 +6,7 @@ case "$(uname -s)" in
     Darwin) RID=osx-arm64;  EXT=dylib ;;
     *)      RID=win-x64;    EXT=dll   ;;
 esac
-OUT="../../src-tauri/sidecars"
+OUT="src-tauri/sidecars"
 mkdir -p "$OUT"
 dotnet publish -c Release -r "$RID" --self-contained true -o "$OUT"
 echo "built listen-for-vrc.$EXT -> $OUT"
